fe3798bc7b0b3bcaaed7a5e3519fe2155761d0e1
chengjiang <15712859926@163.com>
2018-11-05 10:54:52 +0800

    路由koa-router

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 const Koa = require('koa');
+const router = require('koa-router')()
 const app = new Koa()
 
-app.use(async (ctx,next)=>{
-    let stime = new Date().getTime()
-    await next()
-    let etime = new Date().getTime()
-    ctx.response.type = 'text/html'
-    ctx.response.body = '<h1>Hello World</h1>'
-    console.log(`请求地址:${ctx.path},响应时间:${etime-stime}ms`)
+app.use(router.routes())
+
+router.get('/',async(ctx,next)=>{
+    ctx.response.body = `<h1>index page</h1>`
 })
 
-app.use(async (ctx,next)=>{
-    console.log('中间件1 doSoming')
-    await next();
-    console.log('中间件1 end')
+router.get('/home',async(ctx,next)=>{
+    ctx.response.body = '<h1>HOME page</h1>'
 })
 
-app.use(async (ctx,next)=>{
-    console.log('中间件2 doSoming')
-    // await next()
-    console.log('中间件2 end')
+router.get('/404',async(ctx,next)=>{
+    ctx.response.body = '<h1>404 Not Found</h1>'
 })
 
-app.use(async (ctx,next)=>{
-    console.log('中间件3 doSoming')
-    await next()
-    console.log('中间件3 end')
-})
 
 app.listen(3000, () => {
     console.log('server is running at http://localhost:3000')
